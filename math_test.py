51f4422f2aa6f795bb85e146f0df40cd88dcc538
Kevin Koh <kevin.koh.dev@gmail.com>
2021-02-06 13:52:04 -0800

    added other test case for foo

--- math_test.py
+++ math_test.py
@@ -2,10 +2,17 @@ def foo(number1, number2):
   return 0
 
 def test_foo():
-  number1 = 2
-  number2 = 3
-  result = foo(number1, number2)
+  number_a = 2
+  number_b = 3
+  result = foo(number_a, number_b)
   assert result == 5
+
+  number_c = 4
+  number_d = 2
+  result = foo(number_c, number_d)
+  assert result == 6
+
+  print("All tests on foo finished")
 
 if __name__ == '__main__':
   # This calls the test_foo function 
